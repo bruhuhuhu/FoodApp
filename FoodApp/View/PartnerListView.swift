6e68d4871272e486c70fa0e6c38be477a65a0231
bruhuhuhu <hutsunhao@gmail.com>
2018-01-19 18:31:38 +0800

    - added view controller and view files for pages needed

---
+++ FoodApp/View/PartnerListView.swift
@@ -0,0 +1,21 @@
+//
+//  PartnerListView.swift
+//  FoodApp
+//
+//  Created by hu tsun hao on 19/1/18.
+//  Copyright © 2018 hu tsun hao. All rights reserved.
+//
+
+import UIKit
+
+class PartnerListView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
